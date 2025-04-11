ec8abae9ccbe31429959fafe6068f300fda89afe
softgenai <admin@softgen.ai>
2025-04-11 21:10:15 +0000

    fix(setup): Remove redundant project setup commits

---
+++ src/applications/index.tsx
@@ -0,0 +1,3 @@
+cat: can't open '/app/src/applications/index.tsx': No such file or directory<Select value={selectedStatus} onValueChange={setSelectedStatus}>
+  <SelectTrigger className="w-[180px]"></SelectTrigger>
+</Select>

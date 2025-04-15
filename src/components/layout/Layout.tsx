4594fe7a0b45fbac129a0493a03e98fd1c459eb4
softgenai <admin@softgen.ai>
2025-04-15 02:36:55 +0000

    fix(setup): Remove redundant project setup commits

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,18 +4,32 @@ import Footer from "./Footer";
 import { ThemeProvider } from 'next-themes';
 import { UserProvider } from '@/contexts/UserContext';
 import { Toaster } from '@/components/ui/toaster';
+import BackButton from "@/components/common/BackButton";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const router = useRouter();
+  
+  // Don't show back button on home page
+  const showBackButton = router.pathname !== '/';
+  
   return (
     <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
       <UserProvider>
         <div className='flex min-h-screen flex-col'>
           <Header />
-          <main className='flex-1'>{children}</main>
+          <main className='flex-1'>
+            {showBackButton && (
+              <div className='container py-4'>
+                <BackButton />
+              </div>
+            )}
+            {children}
+          </main>
           <Footer />
         </div>
         <Toaster />

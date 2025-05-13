489ed8db7b837a144bde05b9e57148aaae582ded
softgenai <admin@softgen.ai>
2025-05-13 17:26:24 +0000

    fix(layout): Wrap app with Layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 
 import { UserProvider } from '@/contexts/UserContext';
+import Layout from '@/components/layout/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from '@/components/ui/toaster';
@@ -7,7 +8,9 @@ import { Toaster } from '@/components/ui/toaster';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
       <Toaster />
     </UserProvider>
   );

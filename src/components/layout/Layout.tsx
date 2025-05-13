958da2d19fd5eb5a31849467df004a6d074fae10
softgenai <admin@softgen.ai>
2025-05-13 18:52:43 +0000

    Updates

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,9 +2,8 @@
 import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { ThemeProvider } from 'next-themes';
-import { UserProvider } from '@/contexts/UserContext';
-import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 import BackButton from "@/components/common/BackButton";
 import { useRouter } from "next/router";
 
@@ -16,15 +15,15 @@ export default function Layout({ children }: LayoutProps) {
   const router = useRouter();
   
   // Don't show back button on home page
-  const showBackButton = router.pathname !== '/';
+  const showBackButton = router.pathname !== "/";
   
   return (
-    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-      <div className='flex min-h-screen flex-col'>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="flex min-h-screen flex-col">
         <Header />
-        <main className='flex-1'>
+        <main className="flex-1">
           {showBackButton && (
-            <div className='container py-4'>
+            <div className="container py-4">
               <BackButton />
             </div>
           )}

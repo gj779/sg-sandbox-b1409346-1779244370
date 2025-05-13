f712679677654f913aa61d9f945cf7d86f32e61a
softgenai <admin@softgen.ai>
2025-05-13 15:20:05 +0000

    fix(build): Resolve PageNotFoundError for _document by rewriting file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,11 @@
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Softgen AI monitoring script */}
-        <script 
-          src="https://cdn.softgen.ai/script.js" 
-          async 
-          data-softgen-monitoring="true"
-        />
-        <link rel='icon' href='/images/logo.svg' />
-      </Head>
-      <body className="antialiased">
+      <Head />
+      <body>
         <Main />
         <NextScript />
       </body>
